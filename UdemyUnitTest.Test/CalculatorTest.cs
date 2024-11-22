59c3ffb75cb0fe578f869090818592c9efbc5088
Aygün Bayır <turkoaygun@gmail.com>
2024-11-23 02:10:36 +0300

    Add project files.

---
+++ UdemyUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using UdemyUnitTest.App;
+using Xunit;
+
+namespace UdemyUnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            // Act
+            var total = calculator.Add(a, b);
+
+            // Assert
+            Assert.Equal<int>(a + b, total);
+            //Assert.NotEqual<int>(a+b, total);
+        }
+    }
+}
